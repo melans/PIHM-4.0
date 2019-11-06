23e9a2f61abfd9c14ff9c0bb8af623569ba683be
Lele Shu <lele.shu@gmail.com>
2019-11-06 13:58:55 -0800

    UPDATE.

--- src/Model/pihm.hpp
+++ src/Model/pihm.hpp
@@ -1,16 +1,3 @@
-/*******************************************************************************
- * File        : pihm.hpp                                                      *
- * Version     : Oct, 2018 (PIHM++ 4.0)                                        *
- * Function    : PIHM (Penn State Integrated Hydrologic Model                  *
- * Developer of PIHM++ 4.0:      Lele Shu (lele.shu@gmail.com)                 *
- * Developer of PIHM 3.0:        Gopal Bhatt (gopal.bhatt@psu.edu)             *
- * Developer of PIHM 2.0:        Mukesh Kumar (muk139@psu.edu)                 *
- * Developer of PIHM 1.0:        Yizhong Qu   (quyizhong@gmail.com)            *
- *-----------------------------------------------------------------------------*
- *                                                                             *
- *.............................................................................*
- *******************************************************************************/
-
 #ifndef pihm_hpp
 #define pihm_hpp
 
