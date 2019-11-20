e3fca5f640f1bd37aa014103b5703c96c20db16c
Lele Shu <lele.shu@gmail.com>
2019-11-19 16:49:42 -0800

    Clean the code and make it faster a bit.

--- src/ModelData/MD_ElementFlux.cpp
+++ src/ModelData/MD_ElementFlux.cpp
@@ -11,7 +11,7 @@ void Model_Data::fun_Ele_Infiltraion(int i, double t){
 }
 void Model_Data::fun_Ele_surface(int i, double t){
     int j, inabr;
-    double  Ymean, dh, s, CrossA;
+    double  Ymean, dh, s, CrossA, Q;
     double isf, nsf; // Available Y in Surface of this/nabor element    
     isf = uYsf[i] - qEleInfil[i] + qEleExfil[i];
     isf = isf < 0. ? 0. : isf;
@@ -28,29 +28,30 @@ void Model_Data::fun_Ele_surface(int i, double t){
                             Ele[inabr].zmax, nsf,
                             Ele[i].depression);
             if(Ymean <= 0.){
-                QeleSurf[i][j] = 0.;
+                Q = 0.;
             }else{
                 s = dh / Ele[i].Dist2Nabor[j];
                 CrossA = Ymean * Ele[i].edge[j];
                 if(s > 0 && isf <=0){
-                    QeleSurf[i][j] = 0.;
+                    Q = 0.;
                 }else if(s < 0 && nsf <=0){
-                    QeleSurf[i][j] = 0.;
+                    Q = 0.;
                 }else{
-                    QeleSurf[i][j] = ManningEquation(CrossA, Ele[i].avgRough[j], Ymean, s);
+                    Q= ManningEquation(CrossA, Ele[i].avgRough[j], Ymean, s);
                 }
 //                CheckNANi(QeleSurf[i][j], i, "QeleSurf[i][j]");
             } //end of ifelse Avg_Y_Surf < 0.
         } else {
-            QeleSurf[i][j] = 0;
+            Q= 0;
         } // end of if
+        QeleSurf[i][j] = Q;
     } // end of for loop
 }// end of functions
 
 
 void Model_Data::fun_Ele_sub(int i, double t){
     int j, inabr;
-    double  Ymean, dh, Kmean, grad, kei, kej;
+    double  Ymean, dh, Kmean, grad, Q;
     
     for (j = 0; j < 3; j++) {
         inabr = Ele[i].nabr[j] - 1;
@@ -59,22 +60,20 @@ void Model_Data::fun_Ele_sub(int i, double t){
             /* Subsurface Lateral Flux Calculation between Triangular elements Follows */
             /***************************************************************************/
             dh = (uYgw[i] + Ele[i].zmin) - (uYgw[inabr] + Ele[inabr].zmin);
-            if(dh > 0. && uYgw[i] <=EPSILON){
-                QeleSub[i][j] = 0.;
+            if(dh > 0. && uYgw[i] <= EPSILON){
+                Q = 0.;
             }else if(dh < 0. && uYgw[inabr]<= EPSILON){
-                QeleSub[i][j] = 0.;
+                Q = 0.;
             }else{
                 Ymean = avgY_gw(Ele[i].zmin, uYgw[i], Ele[inabr].zmin, uYgw[inabr], 0.002);  
                 grad = dh / Ele[i].Dist2Nabor[j];
-                /* Take care of macropore effect */
-                kei = effKH( uYgw[i], Ele[i].AquiferDepth, Ele[i].macD, Ele[i].macKsatH, Ele[i].geo_vAreaF, Ele[i].KsatH);
-                kej = effKH(uYgw[inabr], Ele[inabr].AquiferDepth, Ele[inabr].macD, Ele[inabr].macKsatH, Ele[inabr].geo_vAreaF, Ele[inabr].KsatH);
                 /* It should be weighted average. However, there is an ambiguity about distance used */
-                Kmean = 0.5 * (kei + kej);
-                QeleSub[i][j] = Kmean * grad * Ymean * Ele[i].edge[j];
+                Kmean = 0.5 * (Ele[i].u_effKH + Ele[inabr].u_effKH);
+                Q = Kmean * grad * Ymean * Ele[i].edge[j];
             }
         } else {
-            QeleSub[i][j] = 0;
+            Q = 0;
         } // end of if
+        QeleSub[i][j] = Q;
     } // end of for loop
 }// end of functions
